7bb8d56df54619567736324943063da1338feaec
Simon de Haan <simon@soocial.com>
2009-12-01 14:54:39 +0200

    progress?

--- alexandria.py
+++ alexandria.py
@@ -1,5 +1,56 @@
 from statemachine import StateMachine
+import random
 
+class ReallyDumbTerminal(object):
+    
+    def format(self, msg, append='\n<- '):
+        return '-> ' + '\n-> '.join(msg.split('\n')) + append
+    
+    def display(self, msg):
+        print self.format(msg, append ='')
+    
+    def read(self, msg=''):
+        return raw_input(self.format(msg))
+    
+
+
+class MenuSystem(object):
+    def __init__(self):
+        self.state = []
+        self.answers = {}
+    
+    def store(self, key, value):
+        self.answers[key] = value
+        return value
+    
+    def callback(self, routine):
+        self.state.append(routine)
+        return self
+    
+    def prompt(self, msg):
+        self.state.append(prompt(msg))
+        return self
+    
+    def display(self, msg):
+        self.state.append(display(msg))
+        return self
+    
+    def loop(self, *items, **kwargs):
+        self.state.append(loop(list(items), **kwargs))
+        return self
+    
+    def dump_store(self):
+        for key, value in self.answers.items():
+            print 'key:', key, 'value:', value
+        return self
+    
+    def run(self, client):
+        self.client = client
+        [routine.send(self) for routine in self.state]
+        return self
+
+
+# from http://www.dabeaz.com/coroutines/Coroutines.pdf
 def coroutine(func): 
     def start(*args,**kwargs): 
         cr = func(*args,**kwargs) 
@@ -10,41 +61,36 @@ def coroutine(func):
 @coroutine
 def prompt(message):
     while True:
+        ms = (yield)                                # wait to be given the menu system instance
+        ms.store(message, ms.client.read(message))  # read input from client and yield it back
+
+
+@coroutine
+def loop(items, randomize=False):
+    while True:
         ms = (yield)
-        yield ms.client.read(message)
+        while items:
+            # randomize order
+            if randomize: random.shuffle(items)
+            item = items.pop()
+            item.send(ms)
 
 
-class ReallyDumbTerminal(object):
-
-    def format(self, msg, append='\n<- '):
-        return '-> ' + '\n-> '.join(msg.split('\n')) + append
-
-    def display(self, msg):
-        print self.format(msg)
-
-    def read(self, msg=''):
-        return raw_input(self.format(msg, append='\n<- '))
-
-
-
-class MenuSystem(object):
-    def __init__(self):
-        self.state = []
-    
-    def callback(self, routine):
-        self.state.append(routine)
-        return self
-    
-    def run(self, client):
-        self.client = client
-        for routine in self.state:
-            result = routine.send(self)
-            print result
-    
-
+@coroutine
+def display(message):
+    while True:
+        ms = (yield)
+        ms.client.display(message)
 
 ms = MenuSystem()
 ms \
-    .callback(prompt('hello!')) \
-    .callback(prompt('dont know!')) \
-    .run(client=ReallyDumbTerminal())
+    .prompt('What is your name?') \
+    .loop(
+        prompt('How old are you?'),
+        prompt('What is your favorite food?'),
+        prompt('What is your favorite colour?'),
+        randomize=True
+    ) \
+    .display('Thank you, goodbye') \
+    .run(client=ReallyDumbTerminal()) \
+    .dump_store()
