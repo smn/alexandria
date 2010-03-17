5893ca7a3be48e496d03b71bdb520f1ec9046672
Simon de Haan <simon@soocial.com>
2010-03-17 16:21:09 +0200

    adding examples from the dev day

---
+++ examples/demo.py
@@ -0,0 +1,16 @@
+from alexandria.core import MenuSystem, prompt, end
+from alexandria.validators import pick_one
+
+def get_menu():
+    return MenuSystem(
+        prompt('What is your favorite browser?', options=(
+        'IE', 'Firefox', 'Chrome', 'Safari', 'Opera', 'Lynx', 'other'
+        ), validator=pick_one),
+        prompt('What is your favorite version control system?', options=(
+            'svn', 'git', 'darcs', 'mercurial', 'cvs'
+        ), validator=pick_one),
+        prompt('What is your favorite development environment?', options=(
+            'netbeans', 'eclipse', 'vim', 'emacs', 'textmate', 'notepad'
+        ), validator=pick_one),
+        end('Thanks! You have completed the quiz')
+    )
---
+++ examples/voting.py
@@ -0,0 +1,19 @@
+from alexandria.core import MenuSystem, prompt, end
+from alexandria.validators import pick_one
+
+def get_menu():
+    return MenuSystem(
+        prompt('Which solution is your favorite?', options=(
+        'Tbl1-Edu', 
+        'Tbl2-Edu', 
+        'Tbl3-Commun. info', 
+        'Tbl4-Mob. consc.', 
+        'Tbl5-Health 1', 
+        'Tbl5-Health 2', 
+        'Tbl6-Sec',
+        'Tbl7-Sec',
+        'Tbl8-Sec',
+        'Tbl8-Edu'
+        ), validator=pick_one),
+        end('Thanks for voting!')
+    )
