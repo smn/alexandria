66c71acc0476f560905ebd2a90c78abaebb4b1e9
Simon de Haan <simon@soocial.com>
2010-08-10 08:41:45 +0200

    moving

--- src/alexandria/sessions/db/urls.py
+++ src/alexandria/sessions/db/urls.py
@@ -1,12 +1,25 @@
 from django.conf.urls.defaults import *
 from django.conf import settings
+from alexandria.sessions.db import views
 
 # Uncomment the next two lines to enable the admin:
 from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    # Example:
+    # (r'^dashboards/', include('dashboards.foo.urls')),
+
+    # Uncomment the admin/doc line below and add 'django.contrib.admindocs' 
+    # to INSTALLED_APPS to enable admin documentation:
+    # (r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    
     (r'^static/(.*)$', 'django.views.static.serve', \
                             {'document_root': settings.MEDIA_ROOT}, "static"),
+    
+    
+    # Uncomment the next line to enable the admin:
     (r'^admin/', include(admin.site.urls)),
+    (r'^data.js', views.json),
+    (r'^$', views.home)
 )
