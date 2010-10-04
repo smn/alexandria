9f2fae9aa8f995476457042c869a0e5436befd68
Simon de Haan <simon@soocial.com>
2010-10-05 01:20:29 +0200

    updated view for demo

--- src/alexandria/sessions/db/views.py
+++ src/alexandria/sessions/db/views.py
@@ -5,7 +5,7 @@ from django.http import HttpResponse
 
 def _get_data():
     data = {}
-    for item in Item.objects.filter(key__in=['name', 'industry', 'expectations']):
+    for item in Item.objects.filter(key__in=['name', 'phone', 'technology', 'useful']):
         question_container = data.setdefault(item.key, {})
         question_container.setdefault(item.value, 0)
         question_container[item.value] += 1
